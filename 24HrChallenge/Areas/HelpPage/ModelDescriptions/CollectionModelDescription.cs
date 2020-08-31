5dd3cce48ab5fad43f635d914c26d9783eeb2810
Matthew VanScoik <vanscoik100@gmail.com>
2020-08-31 17:00:04 -0400

    First Push

---
+++ 24HrChallenge/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace _24HrChallenge.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ 24HrChallenge/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _24HrChallenge.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
