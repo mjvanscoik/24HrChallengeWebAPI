b241d54078ae6251570abe6db8111b4a54d9bb0f
mobara121 <mizueo121@gmail.com>
2020-09-03 09:26:13 -0400

    initial

---
+++ 24HrChallenge.Data/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HrChallenge.Data
+{
+    class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public Guid AuthorId { get; set; }
+        public virtual User Author { get; set; }
+    }
+}
